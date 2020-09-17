5866ce126fa620efe5355fd2d7b3d9ae9752a87c
itachoko <54880769+itachoko@users.noreply.github.com>
2020-09-17 16:35:20 +0900

    2

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 $(function () {
-    //reload when resize
-    var timer = false;
-    var prewidth = $(window).width();
-    $(window).resize(function () {
-        if (timer !== false) {
-            clearTimeout(timer);
-        }
-        timer = setTimeout(function () {
-            var nowWidth = $(window).width();
-            if (prewidth !== nowWidth) {
-                location.reload();
-            }
-            prewidth = nowWidth;
-        }, 200);
-    });
-
     $('.aboutme').click(function () {
             $('.aboutme-popup').slideDown('slow');
     });
